ba7bee37314531efe436777fc1101e492ea3f66b
safinsulthan07 <nsafinsulthan@gmail.com>
2026-01-13 16:49:35 +0530

    Second Commit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import UserInfo from './Components/UserInfo';
+import { useSelector } from 'react-redux';
 export default function App() {
+  let data = useSelector((state) => state.user);
   return (
     <div>
+      <h2>WELCOME MR {data.username} </h2>
       <UserInfo />
     </div>
   );
